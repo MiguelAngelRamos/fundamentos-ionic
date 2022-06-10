846603943d810cc4cdf69dd3aa4be2e968744ec3
Miguel Ramos <return.infox@gmail.com>
2022-06-09 22:19:26 -0400

    bases de ionic iconos y listas

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IComponente } from 'src/app/interfaces/componente';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  componentes: IComponente [] = [
+    {
+      icon: 'book-outline',
+      name: 'about',
+      redirectTo: '/about'
+    },
+    {
+      icon: 'call-outline',
+      name: 'contact',
+      redirectTo: '/contact'
+    }
+  ];
 
   constructor() { }
 
