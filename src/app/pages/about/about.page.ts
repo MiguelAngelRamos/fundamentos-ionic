5c9ad9bccb7653e33d05ad4d9c2624be4f40e50a
Miguel Ramos <return.infox@gmail.com>
2022-06-30 19:48:53 -0400

    Action Sheet

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
   
-  constructor() { }
+  constructor(private actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
   }
+
+  onClick() {
+    this.presentActionSheet();
+  }
+
+  // Método que active nuestro action Sheet
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Opciones',
+      backdropDismiss: false,
+      buttons:[
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash-outline',
+          id: 'delete-button',
+          data: {
+            type: 'delete'
+          },
+          handler: () => {
+            console.log('Delete')
+          }
+        },
+        {
+          text: 'Share',
+          icon: 'share-outline',
+          data: 10,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Play',
+          icon: 'caret-forward-circle-outline',
+          data: 'Data value',
+          handler: () => {
+            console.log('Play');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: 'heart-outline',
+          handler: () => {
+            console.log('Favoritos')
+          }
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close-outline',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked')
+          }
+        }
+      ]
+    });
+    await actionSheet.present();
+
+    // codigo adicional
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log(data);
+    console.log(role);
+
+  }
+
 
 }
